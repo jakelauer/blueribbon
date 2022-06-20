715e6eafbf59a529c1bab03c7be196f849b99ea0
jakelauer <jake.lauer@gmail.com>
2022-06-19 23:47:21 -0700

    feat(init): title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,12 +11,8 @@ export default function Home(props: Props) {
 
   return (
     <main>
+      <h1>ShowRibbon 🎗️</h1>
       <Title>Hello {data[0].username}!</Title>
-      <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-      <p>
-        Follow me on Twitter (
-        <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
-      </p>
     </main>
   );
 }
