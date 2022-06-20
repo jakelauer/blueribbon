93ef7b8fb6f1a0dfdd59b79713e9b271859d2b22
jakelauer <jake.lauer@gmail.com>
2022-06-19 23:45:49 -0700

    feat(init): first commit
    First commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,40 @@
 import type { GatsbyConfig } from 'gatsby';
+import { mongo_atlas_password, mongo_atlas_username } from './keys';
+
+const mongoDbConnectionString = `mongodb+srv://${encodeURIComponent(mongo_atlas_username)}:${encodeURIComponent(mongo_atlas_password)}@showribbon-test.gwiyu.mongodb.net`;
+console.log(mongoDbConnectionString);
 
 const config: GatsbyConfig = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-plugin-graphql-codegen",
+      options: {
+        documentPaths: [
+          "./src/**/*.{ts,tsx}"
+        ]
+      }
+    },
+    {
+      // The name of the plugin
+      resolve: 'gatsby-source-mongodb',
+      options: {
+        typePrefix: "ribbon",
+        // Name of the database and collection where are books reside
+        dbName: 'showribbon',
+        collection: 'accounts',
+        connectionString: mongoDbConnectionString,
+        extraParams: {
+          w: "majority",
+          retryWrites: true
+        }
+      }
+    },
+  ],
   jsxRuntime: `automatic`,
 };
 
 export default config;
+
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import Title from '@/components/Title';
+import { graphql } from 'gatsby';
+import { AllAccountsQuery } from '../../graphql-types';
 
-export default function Home() {
+interface Props {
+  data: AllAccountsQuery;
+}
+
+export default function Home(props: Props) {
+  const data = props.data.allMongodbRibbonAccounts.nodes;
+
   return (
     <main>
-      <Title>Hello TypeScript!</Title>
+      <Title>Hello {data[0].username}!</Title>
       <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
       <p>
         Follow me on Twitter (
@@ -12,3 +20,14 @@ export default function Home() {
     </main>
   );
 }
+
+export const pageQuery = graphql`
+  query AllAccounts {
+    allMongodbRibbonAccounts {
+      nodes {
+        username
+        id
+      }
+    }
+  }
+`;
