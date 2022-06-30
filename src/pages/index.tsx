fdbdb9a0f50e84a26020585c5c677a263cf2d8bd
jakelauer <jake.lauer@gmail.com>
2022-06-30 15:53:48 -0700

    fix(home): fun

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
+import squareLogo from '@/static/logo_square_large.png';
 import AppPage from '@/ui/components/AppPage';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Box, Typography } from '@mui/material';
+import { Box, SxProps, Theme, Typography } from '@mui/material';
 import { navigate } from 'gatsby';
 import React, { useEffect, useState } from 'react';
 
@@ -12,8 +13,35 @@ const Index: React.FC<Props> = () => {
 	const [videoIndex, setVideoIndex] = useState(0);
 
 	useEffect(() => {
-		setVideoIndex(Math.ceil(Math.random() * 7));
+		setVideoIndex(Math.ceil(Math.random() * 5));
 	}, []);
+
+	const Text = (props: { sx?: SxProps<Theme> }) => (
+		<Typography
+			variant="h1"
+			sx={{
+				position: `relative`,
+				background: `white`,
+				zIndex: 2,
+				textAlign: `center`,
+				fontSize: {
+					xs: `4rem !important`,
+					md: `7rem !important`,
+				},
+				pt: 10,
+				pb: 15,
+				...(props.sx ?? {}),
+			}}
+		>
+			The best entry system for competitions.
+			<br />
+			<span style={{ fontStyle: `normal`, filter: `saturate(0)` }}>
+				🏆
+			</span>
+			<br />
+			Absolutely free.
+		</Typography>
+	);
 
 	return (
 		<AppPage title="Home">
@@ -32,7 +60,7 @@ const Index: React.FC<Props> = () => {
 							width: `99%`,
 							height: `99%`,
 							objectFit: `cover`,
-							filter: `brightness(0.9)`,
+							filter: `brightness(0.9) invert(1)`,
 						}}
 						src={`/bg_${videoIndex}.webm`}
 						autoPlay
@@ -41,34 +69,29 @@ const Index: React.FC<Props> = () => {
 					/>
 				)}
 
-				<Typography
-					variant="h1"
+				<Text
 					sx={{
-						position: `relative`,
-						background: `white`,
-						zIndex: 2,
-						textAlign: `center`,
 						color: `black`,
 						mixBlendMode: `screen`,
-						fontSize: {
-							xs: `4rem !important`,
-							md: `7rem !important`,
-						},
-						pt: 10,
-						pb: 15,
-						textShadow: `1px 3px 3px rgba(0,0,0,0.4)`,
 					}}
-				>
-					The best entry system for competitions.
-					<br />
-					<span
-						style={{ fontStyle: `normal`, filter: `saturate(0)` }}
-					>
-						🏆
-					</span>
-					<br />
-					Absolutely free.
-				</Typography>
+				/>
+				<Text
+					sx={{
+						position: `absolute`,
+						top: 0,
+						left: 0,
+						color: `black`,
+						mixBlendMode: `difference`,
+						textShadow: `1px 3px 3px rgba(0,0,0,0.4)`,
+						WebkitTextStrokeColor: `black`,
+						WebkitTextStrokeWidth: 4,
+						filter: `invert(1)`,
+						borderRadius: 10,
+					}}
+				/>
+			</Box>
+			<Box sx={{ display: `flex`, justifyContent: `center` }}>
+				<img width={200} src={squareLogo} />
 			</Box>
 		</AppPage>
 	);
