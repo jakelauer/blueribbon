faeefd263d73554eccf7c00bcad003836628c72f
jakelauer <jake.lauer@gmail.com>
2022-07-16 19:52:17 -0700

    feat(wizard): progress on wizard

---
+++ src/contracts/club/ClubTypes.ts
@@ -0,0 +1,9 @@
+export const ClubTypes = [`dog`, `horse`, `basic`] as const;
+
+export type ClubType = typeof ClubTypes[number];
+
+export const ClubTypeLabels: Record<ClubType, string> = {
+	basic: `Basic`,
+	dog: `Dog`,
+	horse: `Horse`,
+};
--- src/pages/club/create/index.tsx
+++ src/pages/club/create/index.tsx
@@ -1,12 +1,54 @@
 import AppPage from '@/ui/shared/AppPage';
-import { Button, FormGroup, Typography } from '@mui/material';
+import {
+	Button,
+	Card,
+	CardContent,
+	FormGroup,
+	Grid,
+	Typography,
+} from '@mui/material';
+import { grey } from '@mui/material/colors';
+import { useState } from 'react';
+
+import {
+	ClubType,
+	ClubTypeLabels,
+	ClubTypes,
+} from '../../../contracts/club/ClubTypes';
 
 export default function Index() {
+	const [selectedClubType, setSelectedClubType] = useState<
+		ClubType | undefined
+	>();
+
 	return (
 		<AppPage title={`Create a club`}>
 			<Typography variant={`h1`}>Create a Club</Typography>
 			<FormGroup>
 				<Typography variant={`h3`}>Club Type</Typography>
+				<Grid container spacing={2}>
+					{ClubTypes.map((clubType) => (
+						<Grid item xs={4} key={clubType}>
+							<Card
+								sx={{
+									cursor: `pointer`,
+									flex: 1,
+									background:
+										selectedClubType === clubType
+											? grey[100]
+											: `transparent`,
+								}}
+								onClick={() => setSelectedClubType(clubType)}
+							>
+								<CardContent>
+									<Typography>
+										{ClubTypeLabels[clubType]}
+									</Typography>
+								</CardContent>
+							</Card>
+						</Grid>
+					))}
+				</Grid>
 			</FormGroup>
 			<FormGroup>
 				<Typography variant={`h3`}>Club Information</Typography>
